40b704b64ca1a4bf6f686710586c0856e9a8cf94
tadashi-aikawa <syou.maman@gmail.com>
2017-03-10 16:21:39 +0900

    :new: Add change encoding add on

---
+++ addons/response-encoding-converter-addon/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+
+from owlmixin import OwlMixin
+from modules.models import ResponseAddOnPayload
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class Config(OwlMixin):
+    def __init__(self, encoding):
+        self.encoding: str = encoding
+
+
+def main(payload: ResponseAddOnPayload, config_dict: dict):
+    config: Config = Config.from_dict(config_dict or {})
+
+    return ResponseAddOnPayload.from_dict({
+        "response": payload.response,
+        "body": payload.body.decode(payload.encoding).encode(config.encoding),
+        "encoding": config.encoding
+    })
