8f1c5ccf29903c7bd18b3e5bc3515a258063b6a6
tadashi-aikawa <syou.maman@gmail.com>
2017-03-10 15:47:44 +0900

    :hammer: Enhance log output

--- addons/xml-response-parser-addon/__init__.py
+++ addons/xml-response-parser-addon/__init__.py
@@ -33,10 +33,10 @@ def main(payload: ResponseAddOnPayload, config_dict: dict):
     mime_type = content_type.split(';')[0] if content_type else None
 
     if config.force:
-        logger.debug("Forced to xml")
+        logger.debug(f"Forced to xml -- mime_type: {mime_type} -- encoding: {payload.encoding}")
         body = pretty(payload.body.decode(payload.encoding)).encode(payload.encoding)
     elif mime_type in ('text/xml', 'application/xml'):
-        logger.debug(f"Parse as xml. mime_type: {mime_type}")
+        logger.debug(f"Parse as xml -- mime_type: {mime_type} -- encoding: {payload.encoding}")
         body = pretty(payload.body.decode(payload.encoding)).encode(payload.encoding)
     else:
         body = payload.body
