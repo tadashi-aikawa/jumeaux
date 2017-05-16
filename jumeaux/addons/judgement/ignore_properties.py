6cf4cb75ff00f00d6bc746f32a1b2d4fdac4e89c
tadashi-aikawa <syou.maman@gmail.com>
2017-05-17 01:40:44 +0900

    :new: Add `image` and `link` in config [ignore properties add-on]

--- jumeaux/addons/judgement/ignore_properties.py
+++ jumeaux/addons/judgement/ignore_properties.py
@@ -28,32 +28,51 @@ logger = logging.getLogger(__name__)
 
 
 class RegMatcher(OwlMixin):
-    def __init__(self, pattern: str, note: Optional[str]=None):
-        self.pattern: str = pattern
-        self.note: Optional[str] = note
+    pattern: Optional[str]
+    note: Optional[str]
+    image: Optional[str]
+    link: Optional[str]
+
+    def __init__(self, pattern: Optional[str]=None, note: Optional[str]=None,
+                 image: Optional[str]=None, link: Optional[str]=None):
+        self.pattern = pattern
+        self.note = note
+        self.image = image
+        self.link = link
 
 
 class Path(OwlMixin):
+    pattern: str
+    added: TList[RegMatcher]
+    removed: TList[RegMatcher]
+    changed: TList[RegMatcher]
+
     def __init__(self, pattern: str, added: Optional[List[str]]=None, removed: Optional[List[str]]=None, changed: Optional[List[str]]=None):
-        self.pattern: str = pattern
-        self.added: TList[RegMatcher] = RegMatcher.from_optional_dicts(added) if added is not None else TList()
-        self.removed: TList[RegMatcher] = RegMatcher.from_optional_dicts(removed) if removed is not None else TList()
-        self.changed: TList[RegMatcher] = RegMatcher.from_optional_dicts(changed) if changed is not None else TList()
+        self.pattern = pattern
+        self.added = RegMatcher.from_optional_dicts(added) if added is not None else TList()
+        self.removed = RegMatcher.from_optional_dicts(removed) if removed is not None else TList()
+        self.changed = RegMatcher.from_optional_dicts(changed) if changed is not None else TList()
 
 
 class Ignore(OwlMixin):
+    path: Path
+
     def __init__(self, path: dict):
-        self.path: Path = Path.from_dict(path)
+        self.path = Path.from_dict(path)
 
 
 class Config(OwlMixin):
+    ignores: TList[Ignore]
+
     def __init__(self, ignores):
-        self.ignores: TList[Ignore] = Ignore.from_dicts(ignores)
+        self.ignores = Ignore.from_dicts(ignores)
 
 
 class Executor(JudgementExecutor):
+    config: Config
+
     def __init__(self, config: dict):
-        self.config: Config = Config.from_dict(config or {})
+        self.config = Config.from_dict(config or {})
 
     def exec(self, payload: JudgementAddOnPayload):
         if payload.regard_as_same or payload.diff_keys is None:
