9b54d3ce4fa4f3cd5418710194015dbac8b678cc
tadashi-aikawa <syou.maman@gmail.com>
2017-07-12 07:01:28 +0900

    :hammer: Remove legacy url

--- release.sh
+++ release.sh
@@ -2,7 +2,6 @@
 
 python setup.py bdist_wheel
 twine upload dist/jumeaux-${RELEASE_VERSION}-py3-none-any.whl \
-  --repository-url "https://pypi.python.org/pypi" \
   --config-file ".pypirc" \
   -u tadashi-aikawa \
   -p ${PYPI_PASSWORD}
