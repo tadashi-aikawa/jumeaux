850bf09f725454f6d5578f2593ca8d58ffb2f7de
tadashi-aikawa <syou.maman@gmail.com>
2017-12-22 12:01:30 +0900

    :hammer: Add tests

--- tests/addons/reqs2reqs/test_empty_guard.py
+++ tests/addons/reqs2reqs/test_empty_guard.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 import pytest
 from owlmixin import TOption
 from owlmixin.util import load_yaml
 
 from jumeaux.addons.reqs2reqs.empty_guard import Executor
-from jumeaux.models import Reqs2ReqsAddOnPayload
+from jumeaux.models import Reqs2ReqsAddOnPayload, Notifier
 from jumeaux.models import Config as JumeauxConfig
 
 EMPTY = ("Guard empty",
              """
              notifies:
                - notifier: jumeaux
-                 message: notify!
+                 message: "{title} notify!"
              """,
              [],
              """
+             title: empty test
              one:
                 name: one
                 host: http://one   
@@ -28,21 +29,24 @@ EMPTY = ("Guard empty",
                 response_dir: responses
              notifiers:
                 jumeaux:
-                type: slack
-                channel: "#hogehoge"
-                username: hogehoge
+                  type: slack
+                  channel: "#jumeaux"
+                  username: jumeaux
+                dummy:
+                  type: slack
+                  channel: "#dummy"
+                  username: dummy
              addons:
                 log2reqs:
                   name: plain
-             """,
-             []
+             """
              )
 
 NOT_EMPTY = ("Not guard not empty",
          """
          notifies:
            - notifier: jumeaux
-             message: notify!
+             message: "notify!"
          """,
          [
              {"name": "req", "path": "/sample", "headers": {}, "qs": {}}
@@ -66,17 +70,18 @@ NOT_EMPTY = ("Not guard not empty",
          )
 
 
+def send_for_test(message: str, notifier: Notifier) -> TOption[str]:
+    return TOption(message)
+
+
 @patch('jumeaux.addons.reqs2reqs.empty_guard.send')
 class TestExec:
     @pytest.mark.parametrize(
         'title, config_yml, requests, jumeaux_config_yml, expected_result', [
-            EMPTY,
             NOT_EMPTY,
         ]
     )
-    def test(self, send, title, config_yml, requests, jumeaux_config_yml, expected_result):
-        send.side_effect = TOption(None)
-
+    def test_not_use_notify(self, send, title, config_yml, requests, jumeaux_config_yml, expected_result):
         payload: Reqs2ReqsAddOnPayload = Reqs2ReqsAddOnPayload.from_dict({
             'requests': requests,
         })
@@ -85,3 +90,26 @@ class TestExec:
             .exec(payload, JumeauxConfig.from_yaml(jumeaux_config_yml))
 
         assert actual.requests.to_dicts() == expected_result
+
+    @pytest.mark.parametrize(
+        'title, config_yml, requests, jumeaux_config_yml', [
+            EMPTY,
+        ]
+    )
+    def test_use_notify(self, send, title, config_yml, requests, jumeaux_config_yml):
+        send.side_effect = send_for_test
+
+        payload: Reqs2ReqsAddOnPayload = Reqs2ReqsAddOnPayload.from_dict({
+            'requests': requests,
+        })
+
+        try:
+            Executor(load_yaml(config_yml)) \
+                .exec(payload, JumeauxConfig.from_yaml(jumeaux_config_yml))
+        except SystemExit:
+            assert send.call_args[0][0] == 'empty test notify!'
+            assert send.call_args[0][1].to_dict() == {
+                "type": "slack",
+                "channel": "#jumeaux",
+                "username": "jumeaux"
+            }
