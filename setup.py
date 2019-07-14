74fd6adb5843d7f56858fc0a1524cb0a0cb7c8a4
tadashi-aikawa <syou.maman@gmail.com>
2019-07-14 17:20:29 +0900

    :skull: Fix test failure and remove support for Python 3.8

--- setup.py
+++ setup.py
@@ -59,6 +59,5 @@ setup(
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
     ],
 )
